a2bfefa5652dced4687b313727c75760d803443f
HapiFive <1143910315@qq.com>
2023-01-30 00:46:40 +0800

    添加项目文件。

---
+++ PlaintextEncoder/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PlaintextEncoder;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PlaintextEncoder/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PlaintextEncoder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
