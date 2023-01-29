86f1552476cbb817055a7eba5e7f209393fabe78
HapiFive <1143910315@qq.com>
2023-01-30 01:56:28 +0800

    逐字获取拼音测试

--- PlaintextEncoder/MainPage.xaml.cs
+++ PlaintextEncoder/MainPage.xaml.cs
@@ -17,13 +17,16 @@ public partial class MainPage : ContentPage {
         } else {
             CounterBtn.Text = $"Clicked {count} times";
         }
-
+        string[] strings = WordsHelper.GetPinyinList("一骑绝尘", true);
+        for (int i = 0; i < strings.Length; i++) {
+            CounterBtn.Text += strings[i] + "-";
+        }
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
 
     private void InputText_TextChanged(object sender, TextChangedEventArgs e) {
         if (e.NewTextValue.Length > 0) {
-            OutputText.Text = WordsHelper.GetPinyinForName(e.NewTextValue, true);
+            OutputText.Text = WordsHelper.GetPinyin(e.NewTextValue, true);
         } else {
             OutputText.Text = "";
         }
